9096acd9314b528758059024b4e43bc9897e5cbe
rajeshkumar <learnbe4@gmail.com>
2025-04-25 18:58:55 +0530

    Added Hinter

--- entrypoint-jupyter.sh
+++ entrypoint-jupyter.sh
@@ -10,3 +10,8 @@ NOTEBOOK_DIR=${NOTEBOOK_DIR:-'/opt/notebooks'}
 
 # Start JupyterLab
 jupyter lab --ip=0.0.0.0 --port=$JUPYTER_PORT --no-browser --allow-root --NotebookApp.token="$JUPYTER_TOKEN" --notebook-dir="$NOTEBOOK_DIR"
+
+# Add required JupyterLab extensions
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+jupyter nbextension enable hinterland/hinterland
